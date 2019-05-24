8ae07ddbed076c89f5f97fa58078cbeb72bf8ada
Stephen Stewart <stephen.stewart@carisenda.com>
2019-05-25 00:02:20 +0100

    Update frontend/src/LazyInvoice.js

--- frontend/src/LazyInvoice.js
+++ frontend/src/LazyInvoice.js
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
-export const Component = () => <div>Hello Fabien!</div>;
+export const Component = () => <div>Hello Feibian!</div>;
 
 export default hot(Component);
