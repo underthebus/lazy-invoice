73d5454c51def942809bfb6ee635d4f207b1c1fd
Stephen Stewart <stephen.stewart@carisenda.com>
2019-05-27 16:17:56 +0100

    Travis (#5)
    * Added this
    
    * switch folder
    
    * cd before installing
    
    * Fix tests

--- frontend/src/LazyInvoice.js
+++ frontend/src/LazyInvoice.js
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
-export const Component = () => <div>Hello Feibian!</div>;
+export const Component = () => <div data-e2e="example">Hello Feibian!</div>;
 
 export default hot(Component);
