2b0f6d0bc979a407c2bb8cc8690d13bb5a333a27
Fabián Ezequiel Gallina <galli.87@gmail.com>
2019-05-27 14:59:02 -0300

    backend: added inital graphql schema definition (#7)

--- backend/graphql/resolver.go
+++ backend/graphql/resolver.go
@@ -19,12 +19,30 @@ func (r *Resolver) Query() QueryResolver {
 
 type mutationResolver struct{ *Resolver }
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input models.NewTodo) (*models.Todo, error) {
+func (r *mutationResolver) CreateInvoice(ctx context.Context, invoice models.NewInvoice) (*models.Invoice, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) EditInvoice(ctx context.Context, invoice models.EditInvoice) (*models.Invoice, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) DeleteInvoice(ctx context.Context, id string) (string, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) CreateCustomer(ctx context.Context, customer models.NewCustomer) (*models.Customer, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) EditCustomer(ctx context.Context, customer models.EditCustomer) (*models.Customer, error) {
+	panic("not implemented")
+}
+func (r *mutationResolver) DeleteCustomer(ctx context.Context, id string) (string, error) {
 	panic("not implemented")
 }
 
 type queryResolver struct{ *Resolver }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*models.Todo, error) {
+func (r *queryResolver) Invoices(ctx context.Context) ([]*models.Invoice, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Customers(ctx context.Context) ([]*models.Customer, error) {
 	panic("not implemented")
 }
