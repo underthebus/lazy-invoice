ad4b2e119ac7e79dd8720b33ac24dc793e00cf93
Stephen Stewart <stephen.stewart@carisenda.com>
2019-05-30 13:30:17 +0100

    Add assertion timeout

--- frontend/e2e/home/t_index.js
+++ frontend/e2e/home/t_index.js
@@ -5,5 +5,5 @@ fixture `Getting Started`
 
 test('Test home page renders in dev environment', async t => {
   const exampleEl = Selector('[data-e2e="example"]');
-  await t.expect(exampleEl.innerText).eql('Hello Feibian!');
+  await t.expect(exampleEl.innerText).eql('Hello Feibian!', '', { timeout: 1000 });
 });
